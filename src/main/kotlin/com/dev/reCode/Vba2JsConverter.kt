30a2cc65df9b7149b9a64d9bd7d73544f646c26b
Denis <dvoronkovporsche@gmail.com>
2024-01-20 16:39:19 +0500

    Removed unused code example

--- src/main/kotlin/com/dev/reCode/Vba2JsConverter.kt
+++ src/main/kotlin/com/dev/reCode/Vba2JsConverter.kt
@@ -4,65 +4,6 @@ import java.util.*
 
 
 class Vba2JsConverter {
-//    fun main() {
-//        val examples: List<String> = listOf(
-//            """Sub Example()
-//    Dim myRange
-//    Dim result
-//    Dim Run As Long
-//
-//    For Run = 1 To 3
-//        Select Case Run
-//        Case 1
-//            result = "=SUM(A1:A100)"
-//        Case 2
-//            result = "=SUM(A1:A300)"
-//        Case 3
-//            result = "=SUM(A1:A25)"
-//        End Select
-//        ActiveSheet.range("B" & Run) = result
-//    Next Run
-//End Sub""",
-//            """Sub example()
-//    Range("B3").Interior.Color = RGB(0, 0, 250)
-//End Sub""",
-//            """Sub example()
-//    Dim xRg As Range
-//    Dim xTxt As String
-//    Dim xCell As Range
-//    Dim xChar As String
-//    Dim xCellPre As Range
-//    Dim xCIndex As Long
-//    Dim xCol As Collection
-//    Dim I As Long
-//    On Error Resume Next
-//    If ActiveWindow.RangeSelection.Count > 1 Then
-//      xTxt = ActiveWindow.RangeSelection.AddressLocal
-//    Else
-//      xTxt = ActiveSheet.UsedRange.AddressLocal
-//    End If
-//    Set xRg = Application.InputBox("please select the data range:", "Kutools for Excel", xTxt, , , , , 8)
-//    If xRg Is Nothing Then Exit Sub
-//    xCIndex = 2
-//    Set xCol = New Collection
-//    For Each xCell In xRg
-//      On Error Resume Next
-//      xCol.Add xCell, xCell.Text
-//      If Err.Number = 457 Then
-//        xCIndex = xCIndex + 1
-//        Set xCellPre = xCol(xCell.Text)
-//        If xCellPre.Interior.ColorIndex = xlNone Then xCellPre.Interior.ColorIndex = xCIndex
-//        xCell.Interior.ColorIndex = xCellPre.Interior.ColorIndex
-//      ElseIf Err.Number = 9 Then
-//        MsgBox "Too many duplicate companies!", vbCritical, "Kutools for Excel"
-//        Exit Sub
-//      End If
-//      On Error GoTo 0
-//    Next
-//End Sub"""
-//        )
-//        println(vbsToJs(examples[0]))
-//    }
 
     var strs = mutableListOf<String>()
     fun vbsToJs(vbs: String): String {
