8dc78f66f73f9d43c748f8c08569e6f1fd2a6b1d
true-lnz <lansonzgroup@yandex.ru>
2024-01-28 00:27:52 +0500

    Update app.js: add new exist checks

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -5,46 +5,54 @@ document.addEventListener("DOMContentLoaded", function () {
     // Находим элемент кнопки
     var showButton = document.querySelector('.show-button');
     var burgerMenu = document.querySelector('.burger-menu');
-
     var overlay = document.querySelector('.menu-overlay');
+
     // Находим все элементы nav-link
     var navLinks = document.querySelectorAll('.nav-link');
+
     // Устанавливаем значение, при котором кнопка должна появиться
     var scrollThreshold = 300;
 
     // Функция для проверки положения прокрутки и применения стилей
     function checkScroll() {
-        if (burgerMenu.classList.contains('active')) {
-            arrowContainer.style.display = 'none';
-            return;
-        }
-        if (window.scrollY >= scrollThreshold) {
-            // Если прокрутка превысила пороговое значение, добавляем стиль
-            arrowContainer.style.display = 'flex';
-        } else {
-            // В противном случае скрываем кнопку
-            arrowContainer.style.display = 'none';
+        // Проверяем, был ли успешно найден arrowContainer
+        if (arrowContainer) {
+            if (burgerMenu && burgerMenu.classList.contains('active')) {
+                arrowContainer.style.display = 'none';
+                return;
+            }
+            if (window.scrollY >= scrollThreshold) {
+                // Если прокрутка превысила пороговое значение, добавляем стиль
+                arrowContainer.style.display = 'flex';
+            } else {
+                // В противном случае скрываем кнопку
+                arrowContainer.style.display = 'none';
+            }
         }
     }
 
     // Функция для прокрутки страницы к верху при клике на кнопку
     function toggleMenu() {
-        if (burgerMenu.classList.contains('active')) {
-            showButton.querySelector('use').setAttribute('xlink:href', '#show');
-            burgerMenu.classList.remove('active')
-            overlay.style.display = 'none';
-        }
-        else {
-            showButton.querySelector('use').setAttribute('xlink:href', '#hide');
-            burgerMenu.classList.add('active')
-            overlay.style.display = 'block';
+        if (showButton && burgerMenu && overlay) {
+            if (burgerMenu.classList.contains('active')) {
+                showButton.querySelector('use').setAttribute('xlink:href', '#show');
+                burgerMenu.classList.remove('active');
+                overlay.style.display = 'none';
+            } else {
+                showButton.querySelector('use').setAttribute('xlink:href', '#hide');
+                burgerMenu.classList.add('active');
+                overlay.style.display = 'block';
+            }
         }
     }
+
     // Функция скрытия burgerMenu при изменении размера окна
     function handleResize() {
-        showButton.querySelector('use').setAttribute('xlink:href', '#show');
-        burgerMenu.classList.remove('active')
-        overlay.style.display = 'none';
+        if (showButton && burgerMenu && overlay) {
+            showButton.querySelector('use').setAttribute('xlink:href', '#show');
+            burgerMenu.classList.remove('active');
+            overlay.style.display = 'none';
+        }
     }
 
     // Вызываем функцию при загрузке страницы и при каждой прокрутке
@@ -52,7 +60,9 @@ document.addEventListener("DOMContentLoaded", function () {
     checkScroll(); // Проверяем положение прокрутки при загрузке страницы
 
     // Добавляем обработчик клика на кнопку
-    showButton.addEventListener('click', toggleMenu);
+    if (showButton) {
+        showButton.addEventListener('click', toggleMenu);
+    }
 
     // Добавляем обработчик изменения размера окна
     window.addEventListener('resize', handleResize);
