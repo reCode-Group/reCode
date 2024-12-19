16d6d7e987429c37527570277810c37057b2b407
true-lnz <lansonzgroup@yandex.ru>
2024-12-19 23:33:24 +0500

    Add styles for conversions history page

--- src/main/kotlin/com/dev/reCode/controller/mvc/ConversionsController.kt
+++ src/main/kotlin/com/dev/reCode/controller/mvc/ConversionsController.kt
@@ -1,7 +1,11 @@
 package com.dev.reCode.controller.mvc
 import com.dev.reCode.dto.ConversionDto
 import com.dev.reCode.repository.ConversionRepository
+import com.dev.reCode.repository.UserRepository
+import com.dev.reCode.service.UserService
 import org.springframework.data.domain.PageRequest
+import org.springframework.security.core.userdetails.User
+import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
@@ -9,27 +13,9 @@ import org.springframework.web.bind.annotation.RequestParam
 
 @Controller
 class ConversionsController(
-        private val conversionRepository: ConversionRepository // Инъекция репозитория
 ) {
     @GetMapping("/conversions")
-    fun conversions(
-            @RequestParam(defaultValue = "0") page: Int, // Номер страницы
-            @RequestParam(defaultValue = "10") size: Int, // Размер страницы
-            model: Model
-    ): String {
-        // Получение текущего пользователя
-        val user: User = getCurrentUser()
-
-        // Получение страницы данных
-        val pageable: PageRequest = PageRequest.of(page, size)
-        val conversionsPage = conversionRepository.findPageableAllByUser(pageable, user)
-        val conversionsDto = conversionsPage.content.map { ConversionDto(it) }
-
-        // Добавляем данные в модель
-        model.addAttribute("conversions", conversionsDto)
-        model.addAttribute("currentPage", page)
-        model.addAttribute("totalPages", conversionsPage.totalPages)
-
+    fun conversions(): String {
         return "conversions"
     }
 }
